754c20cd10afad1827ab0a813356d12482095631
Mikita Kryvitski <solairerove@gmail.com>
2019-06-22 00:23:33 +0300

    feature: init redux

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 class App extends React.Component {
     render() {
         return (
-            <div>App</div>
+            <div className="ui container">App</div>
         );
     }
 }
