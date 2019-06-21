08e0a9ae1200b662ac202df6bdd1d04771de75a3
Mikita Kryvitski <solairerove@gmail.com>
2019-06-22 00:26:37 +0300

    feature: create stub post list component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import PostList from './PostList';
 
 class App extends React.Component {
     render() {
         return (
-            <div className="ui container">App</div>
+            <div className="ui container">
+                <PostList />
+            </div>
         );
     }
 }
