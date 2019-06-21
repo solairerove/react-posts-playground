f2cd8e832c6b71be16fe89a71d24b376136da30f
Mikita Kryvitski <solairerove@gmail.com>
2019-06-22 01:23:03 +0300

    feature: logger and thunk middleware

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { applyMiddleware, compose, createStore } from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 import App from './components/App';
 import reducers from './reducers';
 
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
     reducers,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeEnhancer(applyMiddleware(thunk), applyMiddleware(logger))
 )
 
 ReactDOM.render(
